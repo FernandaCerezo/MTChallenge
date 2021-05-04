07c8b4ef8d2b935b3fdb969cd0263b8a71028b54
FernandaCerezo <fernandacerezog@gmail.com>
2021-05-03 17:02:48 -0700

    Changed first screen

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,27 +1,67 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { Dimensions, StyleSheet, Text, View, TextInput } from 'react-native';
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
 const styles = StyleSheet.create({
   mainContainer: {
-    alignItems: 'flex-end',
-    backgroundColor: 'blue',
+    alignItems: 'center',
     display: 'flex',
     flex: 1,
-    flexDirection: 'column',
-    height: '100%',
-    justifyContent: 'center',
-    width: '100%',
+    height: windowHeight,
+    justifyContent: 'flex-start',
+    width: windowWidth,
+    fontFamily: 'sans-serif',
   },
   textTitle: {
-    color: 'white',
+    color: 'black',
+    fontSize: 40,
+    fontWeight: 'bold',
+    paddingTop: 70,
+  },
+  textInput: {
+    borderColor: 'gray',
+    borderBottomWidth: 1,
+    color: 'black',
+    fontSize: 18,
+    width: 300,
   },
 });
 
-const AppRouter = () => (
-  <View style={styles.mainContainer}>
-    <Text style={styles.textTitle}>Hola</Text>
-    <Text>♥</Text>
-  </View>
-);
+const AppRouter = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const onChangeEmail = emailValue => {
+    setEmail(emailValue);
+  };
+  const onChangePassword = passwordValue => {
+    setPassword(passwordValue);
+  };
+
+  return (
+    <View style={styles.mainContainer}>
+      <View>
+        <Text style={styles.textTitle}>Please sign in.</Text>
+      </View>
+      <View>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={onChangeEmail}
+          value={email}
+          placeholder="user@user.com"
+          keyboardType="email-address"
+        />
+        <TextInput
+          style={styles.textInput}
+          onChangeText={onChangePassword}
+          value={password}
+          placeholder="password"
+          secureTextEntry
+        />
+      </View>
+    </View>
+  );
+};
 
 export { AppRouter };
